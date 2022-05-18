d8954ffc1a24ecae1e5c666ae8c28cd5272dba5f
PC11 <PC11@INFO5-5382>
2022-05-18 09:29:58 +0200

    Aggiungere i file di progetto.

---
+++ AS2122_4E_INF_GiacchettiManuel_GestioneArticoli/FrmMain.cs
@@ -0,0 +1,10 @@
+namespace AS2122_4E_INF_GiacchettiManuel_GestioneArticoli
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
