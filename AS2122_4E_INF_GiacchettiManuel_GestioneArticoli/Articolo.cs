c66756b1b166bbec3784d82000a48dada258dbf4
manuelgiacchetti28@gmail.com <PC11@INFO5-5382>
2022-05-18 09:48:22 +0200

    creazione classe articolo verifica informatica 18/05/22

---
+++ AS2122_4E_INF_GiacchettiManuel_GestioneArticoli/Articolo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2122_4E_INF_GiacchettiManuel_GestioneArticoli
+{
+    internal class Articolo
+    {
+        string codice;
+        string prezzo;
+        string unitaDimisura;
+        string descrizione;
+
+        string Codice { get { return codice; } }
+
+        string Descrizione { get { return descrizione;} }
+        
+        string UnitaDiMisura { get { return unitaDimisura; } }
+
+        double Prezzo { get { return Prezzo; } }
+
+
+    }
+}
